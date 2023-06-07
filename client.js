0f3b958c0b6689681c14302e095081e7b90b4f05
Paulo Campos Filho <pezaum@gmail.com>
2023-06-07 12:26:49 -0700

    Refactored play.js to modularize the code. Created client.js

---
+++ client.js
@@ -0,0 +1,16 @@
+const net = require('net');
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: 'localhost', // for now
+    port: 50541
+  });
+  // set correct encoding
+  conn.setEncoding('utf-8');
+  conn.on('data', (data) => {
+    console.log('data:', data);
+  });
+  return conn;
+};
+
+module.exports = { connect };
